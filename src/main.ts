32c34ae3e60e88c88129eae62d741748359144d9
antonfuflygin <anton.fuflygin@gmail.com>
2025-08-17 19:38:01 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('next-chat example')
+    .setDescription('The next-chat API description')
+    .setVersion('1.0')
+    .addTag('next-chat')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
